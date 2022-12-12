f5bd5469f28f0e55f23ddf83225f89b05910fae3
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2022-12-12 17:16:00 +0100

    put function

--- src/12.rb
+++ src/12.rb
@@ -1,57 +1,56 @@
 require "matrix"
 
-pending = Queue.new
 lines = []
 line = gets
 while line
   line = line.strip
-  start_tmp = line.index('S')
-  if !start_tmp.nil?
-    pending << [lines.length, start_tmp, 0]
-    line[start_tmp] = 'a'
+  if !line.index('S').nil?
+    start_i, start_j = [lines.length, line.index('S')]
   end
-  end_tmp = line.index('E')
-  if !end_tmp.nil?
-    end_i, end_j = [lines.length, end_tmp]
-    line[end_tmp] = 'z'
+  if !line.index('E').nil?
+    end_i, end_j = [lines.length, line.index('E')]
   end
   lines.append(line)
   line = gets
 end
 
-best = 99999
+def get_height(mychar)
+  if mychar == 'S'
+    mychar = 'a'
+  elsif mychar == 'E'
+    mychar = 'z'
+  end
+  return mychar.ord - 'a'.ord
+end
+  
 
-for start_i in (0..lines.length-1)
-  for start_j in (0..lines[0].length-1)
-    if lines[start_i][start_j] != 'a'
+def bfs(lines, start_i, start_j, end_symbol, order=1)
+  pending = Queue.new
+  pending << [start_i, start_j, 0]
+
+  steps = Matrix.zero(lines.length, lines[0].length)
+  
+  while pending.length > 0
+    i, j, s = pending.pop
+    if lines[i][j] == end_symbol
+      return s
+    end
+    if steps[i, j] > 0 and s >= steps[i, j]
       next
     end
-    
-    pending = Queue.new
-    pending << [start_i, start_j, 0]
-
-    steps = Matrix.zero(lines.length, lines[0].length)
-    
-    while pending.length > 0
-      i, j, s = pending.pop
-      if steps[i, j] == 0 or steps[i, j] < s
-        steps[i, j] = s
-        if i==end_i and j==end_j
-          if s < best
-            best = s
-          end
-          break
-        end
-        for i2, j2 in [[i+1,j], [i-1,j], [i,j+1], [i,j-1]]
-          if 0 <= i2 and i2 < lines.length and 0 <= j2 and j2 < lines[0].length and (steps[i2,j2]==0 or s+1 < steps[i2,j2])
-            height = lines[i2][j2].ord - 1
-            if height <= lines[i][j].ord
-              pending << [i2, j2, s+1]
-            end
-          end
+    steps[i, j] = s
+    for i2, j2 in [[i+1,j], [i-1,j], [i,j+1], [i,j-1]]
+      if 0 <= i2 and i2 < lines.length and 0 <= j2 and j2 < lines[0].length and (steps[i2,j2]==0 or s+1 < steps[i2,j2])
+        if order==1 and get_height(lines[i2][j2])-1 <= get_height(lines[i][j])
+          pending << [i2, j2, s+1]
+        elsif order==2 and get_height(lines[i][j])-1 <= get_height(lines[i2][j2])
+          pending << [i2, j2, s+1]
         end
       end
     end
   end
+  return -1
 end
-puts best
+
+puts bfs(lines, start_i, start_j, 'E')
+puts bfs(lines, end_i, end_j, 'a', 2)
